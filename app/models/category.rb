64a29c48504ba9c6685ee59e354b9316fec6c0dd
Xuncheng Wang <w.xun.cheng@gmail.com>
2015-09-23 17:30:44 +0800

    show only recent videos in the homepage

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
-  has_many :videos, dependent: :destroy
+  has_many :videos, -> { order(created_at: :desc) }, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+
+  def recent_videos
+    videos.limit(6)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,35 @@ RSpec.describe Category, type: :model do
   it { is_expected.to have_many(:videos) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
+
+  describe "#recent_videos" do
+    let(:comedies) { create(:category, name: "Comedies") }
+
+    it "returns videos in a descending order for created_at" do
+      family_guy = create(:video, category: comedies, created_at: 1.day.ago)
+      south_park = create(:video, category: comedies)
+      expect(comedies.recent_videos).to eq([south_park, family_guy])
+    end
+
+    it "returns all videos if there are less than 6 videos" do
+      family_guy = create(:video, category: comedies, created_at: 1.day.ago)
+      south_park = create(:video, category: comedies)
+      expect(comedies.recent_videos.size).to eq(2)
+    end
+
+    it "returns 6 videos if there are more than 6 videos" do
+      7.times { create(:video, category: comedies) }
+      expect(comedies.recent_videos.size).to eq(6)
+    end
+
+    it "returns the latest 6 videos" do
+      family_guy = create(:video, category: comedies, created_at: 1.day.ago)
+      6.times { create(:video, category: comedies) }
+      expect(comedies.recent_videos).not_to include(family_guy)
+    end
+
+    it "returns an empty array if there are no videos" do
+      expect(comedies.recent_videos).to eq([])
+    end
+  end
 end
