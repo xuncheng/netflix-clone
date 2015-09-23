56b97b25975c33b568165eb6f2c92ede00a77c0b
Xuncheng Wang <w.xun.cheng@gmail.com>
2015-09-23 17:05:44 +0800

    implement search on the  UI

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,12 @@ class VideosController < ApplicationController
   def index
     @categories = Category.all.includes(:videos)
   end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def search
+    @result = Video.search_by_title(params[:search_term])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "videos#index"
+  get "home", to: "videos#index"
+
+  resources :videos, only: :show do
+    post :search, on: :collection
+  end
 
   get "ui(/:action)", controller: "ui" if Rails.env.development?
 end
