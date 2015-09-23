2b12f94222161b32f8acc9395a844d6894107481
Xuncheng Wang <w.xun.cheng@gmail.com>
2015-09-23 17:08:56 +0800

    add category show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :videos, only: :show do
     post :search, on: :collection
   end
+  resources :categories, only: :show
 
   get "ui(/:action)", controller: "ui" if Rails.env.development?
 end
